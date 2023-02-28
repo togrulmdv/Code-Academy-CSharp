b9a05476d0675d792889ea4ff3bf8b2e6319ffeb
TogrullM <125910346+TogrullM@users.noreply.github.com>
2023-02-28 15:54:07 +0400

    Add files via upload

---
+++ Program(Methods).cs
@@ -0,0 +1,48 @@
+﻿#region Task 1
+//static int SquareOfNumber(int number)
+//{
+//    return number*number;
+//}
+//Console.Write("Please, enter a number:"); 
+//int EnteredNum=int.Parse(Console.ReadLine());
+//Console.WriteLine("Square of the entered number:"+SquareOfNumber(EnteredNum));
+#endregion
+#region Task 2
+//class program
+//{
+//    public static void Main()
+//    {
+//        Console.WriteLine(Sahe(2,3,4,5));
+//    }
+//    static ulong Sahe(ulong r, ulong pi = 3)
+//    {
+//        return pi * r * r;
+//    }
+
+//    static int Sahe(int a, int b)
+//    {
+//        return a * b;
+//    }
+
+//    static double Sahe(double r, double a, double b, double c)
+//    {
+//        double p = (a + b + c) / 2;
+//        return p * r;
+//    }
+//}
+#endregion
+#region Task 3
+//static int SumUntillNumber(int number)
+//{
+//    int sum = 0;
+//    int a = 1;
+//    while(sum< number)
+//    {
+//        sum += a;
+//        a++;
+//    }
+//    sum -= a-1;
+//    return sum;
+//}
+//Console.WriteLine(SumUntillNumber(8));
+#endregion
