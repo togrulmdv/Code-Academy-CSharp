327c6dacee68df693da288917c04dfbdbf51ea69
TogrullM <125910346+TogrullM@users.noreply.github.com>
2023-03-05 23:28:10 +0400

    task2-03-06

---
+++ task2-03-06/Program.cs
@@ -0,0 +1,19 @@
+﻿static string DeleteSpace(string sentence)
+{
+    char space = ' ';
+    char nospace = '\0';
+    string newone = string.Empty;
+    for (int i = 0; i < sentence.Length; i++)
+    {
+        if (sentence[i] == space)
+        {
+            newone = newone + nospace;
+        }
+        else
+        {
+            newone = newone + sentence[i];
+        }
+    }
+    return newone;
+}
+Console.WriteLine(DeleteSpace("Hello World"));
