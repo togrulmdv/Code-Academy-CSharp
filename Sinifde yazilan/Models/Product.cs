1dc8e26a2012bce7d88d97760fffb92ccffeb5a1
TogrullM <125910346+TogrullM@users.noreply.github.com>
2023-03-05 12:32:17 +0400

    Sinifde yazilan

---
+++ Sinifde yazilan/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sinifde_yazilan.Models
+{
+    public class Product
+    {
+        public string productName;
+        public int costPrice;
+        public int saledPrice;
+    public Product(string productName, int costPrice, int saledPrice)
+        {
+            this.productName = productName;
+            this.costPrice = costPrice;
+            this.saledPrice = saledPrice;
+        }
+    }
+}
---
+++ Sinifde yazilan/Program.cs
@@ -0,0 +1,10 @@
+﻿using Sinifde_yazilan.Models;
+
+public class Program
+{
+    public static void Main()
+    {
+        Book Info = new Book("Sade Heyat", 25, 50, "Memmedaga", 128, 50);
+        Info.GetInfo();
+    }
+}
