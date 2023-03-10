5bffcb1d85369cceeb2a5415ff9f30f60febd797
TogrullM <125910346+TogrullM@users.noreply.github.com>
2023-03-10 14:36:39 +0400

    task-03-10

---
+++ task-03-10/Models/Student.cs
@@ -0,0 +1,31 @@
+﻿namespace task_03_10.Models;
+
+public class Student:Person
+{
+    private byte _point;
+    public byte Point
+    {
+        get { return _point; }
+        set
+        {
+            if (value >= 0 && value <= 100)
+            {
+                _point = value;
+            }
+            else
+            {
+                _point = 0;
+            }
+        }
+    }
+    public Student(string NameDetermine, string SurnameDetermine, byte age, byte PointDetermine) : base(NameDetermine, SurnameDetermine, age)
+    {
+        Point= PointDetermine;
+    }
+}
+    
+
+
+
+
+
---
+++ task-03-10/Program.cs
@@ -0,0 +1,5 @@
+﻿using task_03_10.Models;
+Student adam1 = new("togrul", "mammadov", 19, 96);
+Student adam2 = new("qabil", "turkoglu", 33, 75);
+Console.WriteLine(adam1.Name);
+Console.WriteLine(adam2.Point);
