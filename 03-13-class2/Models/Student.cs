43d392a15eee4adbeb2c84a5a3e26b1ff25ec386
TogrullM <125910346+TogrullM@users.noreply.github.com>
2023-03-13 21:20:56 +0400

    03-13-class2

---
+++ 03-13-class2/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿namespace class2_03_13.Models;
+
+internal class Student
+{
+    private  static int _id;
+    public int Id { get; }
+    public string FullName { get; set; }
+    public int Point { get; set; }
+    public Student(string fullName, int point)
+    {
+        _id++;
+        Id= _id;
+        FullName = fullName;
+        Point = point;
+    }
+    public void ShowInfo()
+    {
+        Console.WriteLine($"{FullName}, {Point},{Id}");
+    }
+}
---
+++ 03-13-class2/Program.cs
@@ -0,0 +1,6 @@
+﻿using class2_03_13.Models;
+
+Student stu1 = new("Mammadov",18);
+Student stu2 = new("Mahmudov", 28);
+stu1.ShowInfo();
+stu2.ShowInfo();
