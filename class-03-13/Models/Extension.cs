2bcd6cee5de0327bb2907cb2d26d3cacb1d5e055
TogrullM <125910346+TogrullM@users.noreply.github.com>
2023-03-13 20:54:22 +0400

    class-03-13

---
+++ class-03-13/Models/Extension.cs
@@ -0,0 +1,28 @@
+﻿namespace class_03_13.Models;
+
+internal static class Extension
+{
+    public static bool IsOdd(this int number)
+    {
+        return number % 2 != 0;
+    }
+    public static bool IsEven(this int number)
+    {
+        return number % 2 == 0;
+    }
+    public static bool IsContainsDigit(this string word)
+    {
+        bool have=false;
+        for (int i=0;i<word.Length;i++)
+        {
+
+            if (char.IsDigit(word[i]))
+            {
+                have = true;
+            }
+
+        }
+        return have;
+
+    }
+}
