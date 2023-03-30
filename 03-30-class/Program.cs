0962a5ddb14ae8154f6d2d3e5377a13f34e8073a
TogrullM <125910346+TogrullM@users.noreply.github.com>
2023-03-30 19:56:28 +0400

    03-30-class

---
+++ 03-30-class/Program.cs
@@ -0,0 +1,27 @@
+﻿void PrintMessage(string txt) { Console.WriteLine(txt); }
+void PrintMessageUpperCase(string txt) { Console.WriteLine(txt.ToUpper()); }
+void PrintMessageReverse(string txt) 
+{
+    string newoone=string.Empty;
+    for(int i=txt.Length;i<0;i--)
+    {
+        newoone += txt[i];
+    }
+    Console.WriteLine(newoone);
+}
+
+
+
+Print printele = PrintMessage;
+printele += PrintMessageReverse;
+printele += PrintMessageUpperCase;
+
+
+
+
+
+string word = "asd";
+printele(word);
+
+
+delegate void Print(string txt);
